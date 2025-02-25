82f43768ab459d0500e17e1d97efb92387d790ff
BronzeRunner <qaul38@gmail.com>
2025-02-25 09:04:59 +0900

    Reset !

--- Scripts/FightSystem/Identity_Stat.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Identity_Stat", menuName = "Scriptable Objects/Identity_Stat")]
-public class Identity_Stat : ScriptableObject
-{
-    //public int Identity_Level = 1;
-   
-}
